abdb8d3745e9bb5203c3ba60a60e84ed14a7deb6
delarita <j.delarita@gmail.com>
2025-03-19 14:59:35 +0100

    divers

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -69,7 +69,11 @@ class NotesController < ApplicationController
 
     # Use callbacks to share common setup or constraints between actions.
     def set_note
-      @note = Note.find(params.expect(:id))
+      if Note.exists?(params.expect(:id))
+        @note = Note.find(params.expect(:id))
+      else
+        redirect_to root_path, alert: "Note not found"
+      end
     end
 
     # Only allow a list of trusted parameters through.
